f5b545f2dd8d45caca6076a646149a002402a71f
getto-dev <sailordag@gmail.com>
2026-09-19 21:47:21 +0300

    fix: fit four digit quantities in estimate input (#11)

--- src/components/EstimateTable.tsx
+++ src/components/EstimateTable.tsx
@@ -173,7 +173,7 @@ export const EstimateTable: React.FC<EstimateTableProps> = ({
                                   ariaLabel={`Количество: ${item.name}`}
                                   title="Количество, шаг 0.5"
                                   enterKeyHint="done"
-                                  className="w-14 border-0 bg-transparent px-1 text-center sm:w-10"
+                                  className="w-16 border-0 bg-transparent px-1 text-center sm:w-16"
                                 />
                                 <button
                                   type="button"
